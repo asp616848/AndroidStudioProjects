56049510cef8f7cd2c41ecbca04ba8fa2e53550c
asp616848 <asp616848@gmail.com>
2024-01-01 21:16:57 +0530

    app bar aka tab layout added

--- slideNavigation/app/build.gradle.kts
+++ slideNavigation/app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation(libs.androidx.viewpager2)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.appcompat)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- slideNavigation/app/src/main/java/com/example/slidenavigation/MainActivity.kt
+++ slideNavigation/app/src/main/java/com/example/slidenavigation/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.slidenavigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +13,13 @@ class MainActivity : AppCompatActivity() {
         val viewPager = findViewById<ViewPager2>(R.id.view_pager)
         val adapter = SimpleFragmentPageAdapter(supportFragmentManager, lifecycle)
         viewPager.adapter = adapter
+        val tabLay = findViewById<TabLayout>(R.id.tab_layout)
+        TabLayoutMediator(tabLay, viewPager) { tab, position ->
+            when (position) {
+                0 -> tab.text = "Tab 1"
+                1 -> tab.text = "Tab 2"
+                2 -> tab.text = "Tab 3"
+            }
+        }.attach()
     }
 }
