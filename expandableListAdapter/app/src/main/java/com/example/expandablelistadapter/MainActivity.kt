4cf01002582065350c25d267c4416ddd4b51d74d
asp616848 <asp616848@gmail.com>
2024-01-02 15:29:10 +0530

    Added new spinner view UI to the bottom of existing practive project of expandable list view

--- expandableListAdapter/app/src/main/java/com/example/expandablelistadapter/MainActivity.kt
+++ expandableListAdapter/app/src/main/java/com/example/expandablelistadapter/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.expandablelistadapter
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.ExpandableListAdapter
 import android.widget.ExpandableListView
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -16,12 +20,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.expandablelistadapter.ui.theme.ExpandableListAdapterTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), AdapterView.OnItemSelectedListener {
 
     private lateinit var expandableListView: ExpandableListView
     private lateinit var expandableListAdapter: ExpandableListAdapter
     private lateinit var listTitles: List<String>
     private lateinit var detailList: HashMap<String, List<String>>
+    var courses = arrayOf<String?>("C", "Data structures",
+        "Interview prep", "Algorithms",
+        "DSA with java", "OS")
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,5 +56,38 @@ class MainActivity : ComponentActivity() {
             false
         }
 
+        //SPINNER
+
+        val spin = findViewById<Spinner>(R.id.spinner)
+        spin.onItemSelectedListener = this
+
+        // Create the instance of ArrayAdapter
+        // having the list of courses
+        val ad: ArrayAdapter<*> = ArrayAdapter<Any?>(
+            this,
+            android.R.layout.simple_spinner_item,
+            courses)
+
+        // set simple layout resource file
+        // for each item of spinner
+        ad.setDropDownViewResource(
+            android.R.layout.simple_spinner_dropdown_item)
+
+        // Set the ArrayAdapter (ad) data on the
+        // Spinner which binds data to spinner
+        spin.adapter = ad
     }
+
+    override fun onItemSelected(parent: AdapterView<*>?,
+                                view: View, position: Int,
+                                id: Long) {
+        // make toastof name of course
+        // which is selected in spinner
+        Toast.makeText(applicationContext,
+            courses[position],
+            Toast.LENGTH_LONG)
+            .show()
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {}
 }
