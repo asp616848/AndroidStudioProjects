aca71325453454ade38ae7ff81305ef9c77f401d
asp616848 <asp616848@gmail.com>
2024-01-03 22:17:44 +0530

    added features

--- expandableListAdapter/app/src/main/java/com/example/expandablelistadapter/DatePicker.kt
+++ expandableListAdapter/app/src/main/java/com/example/expandablelistadapter/DatePicker.kt
@@ -1,5 +1,6 @@
 package com.example.expandablelistadapter
 
+import android.app.DatePickerDialog
 import android.app.DatePickerDialog.OnDateSetListener
 import android.app.Dialog
 import android.app.DialogFragment
@@ -13,6 +14,6 @@ class DatePicker : DialogFragment() {
         val year = myCalendar.get(Calendar.YEAR)
         val month = myCalendar.get(Calendar.MONTH)
         val dayOfMonth = myCalendar.get(Calendar.DAY_OF_MONTH)
-        return android.app.DatePickerDialog(activity, activity as OnDateSetListener, year, month, dayOfMonth)
+        return DatePickerDialog(activity, activity as OnDateSetListener, year, month, dayOfMonth)
     }
 }
