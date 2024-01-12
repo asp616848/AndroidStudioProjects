3bd99f91a01bf7c97029d24e3657e646bf865807
asp616848 <asp616848@gmail.com>
2024-01-12 10:32:35 +0530

    json parsing

---
+++ Volley/app/src/main/java/com/example/volley/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.volley
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.volley.ui.theme.VolleyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        }
+    }
+}
