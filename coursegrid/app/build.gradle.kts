0dd4234848d0b7c20a0bacf9f2fed54e82e705b6
asp616848 <asp616848@gmail.com>
2024-01-24 11:23:12 +0530

    update

--- coursegrid/app/build.gradle.kts
+++ coursegrid/app/build.gradle.kts
@@ -65,5 +65,6 @@ dependencies {
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
+    implementation("androidx.compose.material:material-icons-extended")
     debugImplementation(libs.androidx.ui.test.manifest)
 }
