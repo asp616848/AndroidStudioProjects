f679961c9f16a4403ba6c9f421fb54a099c94ef9
asp616848 <asp616848@gmail.com>
2024-02-06 18:44:01 +0530

    up

--- MyFamily/app/src/main/java/com/example/myfamily/MainActivity.java
+++ MyFamily/app/src/main/java/com/example/myfamily/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myfamily;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,44 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editUsername;
+    Button btnProceed;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        editUsername = findViewById(R.id.edtUsername);
+        btnProceed = findViewById(R.id.btnProceed);
+        btnProceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Username = editUsername.getText().toString().trim();
+                if(!Username.isEmpty()){
+                    proceedService(Username);
+                }
+                else{
+                    return;
+                }
+            }
         });
+    }
+
+    void proceedService(String userID){
+        PermissionX.init(activityContext).permissions(permission.SYSTEM_ALERT_WINDOW)
+                .onExplainRequestReason(new ExplainReasonCallback() {
+                    @Override
+                    public void onExplainReason(@NonNull ExplainScope scope, @NonNull List<String> deniedList) {
+                        String message = "We need your consent for the following permissions in order to use the offline call function properly";
+                        scope.showRequestReasonDialog(deniedList, message, "Allow", "Deny");
+                    }
+                }).request(new RequestCallback() {
+                    @Override
+                    public void onResult(boolean allGranted, @NonNull List<String> grantedList,
+                                         @NonNull List<String> deniedList) {
+                    }
+                });
+
     }
 }
